2c65b799c623ac010bd031a8915ebe982b6573a9
cthziun <tzuchin2004@gmail.com>
2025-02-26 01:56:36 +0800

    fix getCommitment, askEncryptedPairs, getProof

--- src/libs/getCommitment.ts
+++ src/libs/getCommitment.ts
@@ -1,51 +1,25 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import { API_URL } from '../constants';
+import { commitment } from '../interfaces/objects';
 
 function GetCommitment() {
-    const [commitment, setCommitment] = useState<string | null>(null);
-    const [seed, setSeed] = useState<number | null>(null);
-    const [proof, setProof] = useState<[[number, number], [number, number]] []>([]);
-    const [error, setError] = useState<string | null>(null);
+    let commitment: commitment | undefined;
 
-    useEffect(() => {
-        const getCommitment = async () => {
-            try {
-                await axios.get('https://127.0.0.1:8000/VA/get_pairs');
+    const getCommitment = async () => {
+        try {
+            await axios.get(`${API_URL}/VA/get_pairs`);
 
-                const response = await axios.get('https://127.0.0.1:8000/VA/get_commitment');
-                if (response.status === 200) {
-                    setCommitment(response.data.commitment); // 200
-                    setSeed(response.data.seed);
-                    setProof(response.data.proof);
-                } else {
-                    setError('Failed to get commitment');
-                }
-            } catch (error) {
-                if (axios.isAxiosError(error)) {
-                    setError(error.message);
-                }
+            const response = await axios.get(`${API_URL}/VA/get_commitment`);
+            if (response.status === 200) {
+                commitment = response.data;
             }
-        };
-    }, []);
+        } catch (error) {
+            console.error('Error fetching commitment:', error);
+        }
+    };
 
-    return (
-        <div>
-            <h1>Commitment</h1>
-            {error ? (
-                <p>{error}</p>
-            ) : (
-                <p>Commitment: {commitment}</p>
-                <p>Seed: {seed}</p>
-                <ul>
-                    {proof.map((pair, index) => (
-                        <li key={index}>
-                            {`[(${pair[0][0]}, ${pair[0][1]}), (${pair[1][0]}, ${pair[1][1]})]`}
-                        </li>
-                    ))}
-                </ul>
-            )}
-        </div>
-    );
+    return commitment;
 }
 
 export default GetCommitment;
